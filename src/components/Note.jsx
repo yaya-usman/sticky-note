7200b2e7fd27dac33385b5911e3cde0bb9b16f94
Elixir-K <yayaazeez222@gmail.com>
2021-09-25 23:27:23 +0300

    note component design complete

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
+import React,{useRef,useEffect,useState} from 'react'
+import './note.css';
+import AddCircleIcon from "@mui/icons-material/AddCircle";
 
 function Note() {
+    const focusRef = useRef();
+    const [noteText, setNoteText] = useState('');
+
+    //aiding in focusing the input field automatically
+    useEffect(() => {
+        focusRef.current.focus();
+    }, [])
+
+    const addNote = ()=>{
+    }
+
     return (
-        <form className = "note">
-            <textarea placeholder ="Create a new note...." ></textarea>
-            <button>Add note</button>
-        </form>
-    )
+      <form className="note">
+        <textarea value = {noteText} onChange = {(e) => setNoteText(e.target.value)} placeholder="Create a new note...." ref={focusRef}></textarea>
+        <div onSubmit={addNote}>
+          <AddCircleIcon className="add-btn" />
+        </div>
+      </form>
+    );
 }
 
 export default Note;
